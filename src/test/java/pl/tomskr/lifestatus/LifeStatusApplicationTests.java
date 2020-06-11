0d8a8412124bb2189bd4b4251bff9d4083df451d
TomaszSkrzypek <tomskr3.ts@gmail.com>
2020-06-11 21:16:47 +0200

    Initial commit

---
+++ src/test/java/pl/tomskr/lifestatus/LifeStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomskr.lifestatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeStatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
